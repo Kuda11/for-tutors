34f03950498071d440366360865bace6d1f6eff6
Kuda11 <kudathomad17@yahoo.com>
2020-10-23 20:04:24 +0100

    implemeting packages

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import styles from './App.module.scss'
+
+function App() {
+  return (
+    <div className={styles.body} >
+      I am working
+    </div>
+  );
+}
+
+export default App;
